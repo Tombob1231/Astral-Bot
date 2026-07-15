09ac0e51fa5607f4638e4956f8efe2717e6aa8d1
Tombob1231 <tfart.3134@gmail.com>
2026-07-15 13:22:37 +0100

    Add Rocket League roster with rank emojis

--- commands/roster.js
+++ commands/roster.js
@@ -17,19 +17,19 @@ module.exports = {
             .setImage("https://cdn.discordapp.com/attachments/1526902981516333186/1526914184489472190/Astral_Roster.png?ex=6a58c126&is=6a576fa6&hm=77db93beb7239f023f795fa42990503959abe56b41dc8af40c8e8b26cf5bd90c&")
   .addFields(
     {
-        name: "<:SSL:1526912212936036382> SSL",
+        name: "<:SSL:1526926297534824638> SSL",
         value: "👑 Player 1\n👑 Player 2"
     },
     {
-        name: "<:GC2:1526913140829192244> <:GC3:1526912403407896588> GC2 - GC3",
+        name: "<:GC2:1526926521313394809> - <:GC3:1526926510538231899> GC2 - GC3",
         value: "👤 Player 1\n👤 Player 2\n👤 Player 3"
     },
     {
-        name: "<:C3:1526912529803251722> <:GC1:1526913207053062145> C3 - GC1",
+        name: "<:C3:1526926544524673166> - <:GC1:1526926531618934995> C3 - GC1",
         value: "👤 Player 1\n👤 Player 2\n👤 Player 3"
     },
     {
-        name: "<:C1:1526912879335440495> <:C2:1526912991940051135> C1 - C2",
+        name: "<:C1:1526926561738231819> - <:C2:1526926553689100328> C1 - C2",
         value: "👤 Player 1\n👤 Player 2\n👤 Player 3"
     }
 )
